87b8b119c2ea65288275bf8e0da73e511dccb3dc
Sébastien HOUZÉ <sebastien.houze@french-hospitality.fr>
2023-05-05 15:47:28 +0200

    fix(bref): ggll.localhost ip
    see https://github.com/curl/curl/commit/b5c0fe20e370ea2e5791fce4cedb34a71bc784e5
    
    see https://github.com/curl/curl/issues/9192

--- bref/entrypoint.sh
+++ bref/entrypoint.sh
@@ -3,4 +3,8 @@ set -e
 
 [ $(id -u) -eq 0 ] && update-ca-trust && cp /etc/ssl/certs/ca-bundle.trust.crt /opt/bref/ssl/cert.pem
 
-exec /bref-entrypoint.sh $@
+GGLL_LOCALHOST_IP=$(dig +short -t A ggll.localhost)
+
+echo "export GGLL_LOCALHOST_IP=$GGLL_LOCALHOST_IP" >> ~/.bashrc
+
+exec env GGLL_LOCALHOST_IP=$GGLL_LOCALHOST_IP /bref-entrypoint.sh "$@"
